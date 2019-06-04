96a39af029a44d1190c1db8039b9661e8c5a591a
admin <you@example.com>
2019-06-04 14:17:04 +0530

    third commit

--- javascript/home.js
+++ javascript/home.js
@@ -1,21 +1,4 @@
-// var day= new Date();
-// var hr=day.getHours();
-// if(hr>0 && hr<12){
-// 	document.write("good morning");
-// }
-// else if(hr>=12 && hr<16){
-// 	document.write("good afertnoon");
-// }
-// else if(hr>16 && hr<20)
-// {
-// 	document.write("good evening");
-// }
-// else{
-// 	document.write("good night");
-// }
-
 var d = new Date();
-// document.getElementById("demo").innerHTML = d;
 var myDate = new Date();
     var hrs = myDate.getHours();
 
@@ -32,11 +15,69 @@ var myDate = new Date();
         '<b>' + greet + ',</b> User';
 
         var date = new Date();
-// get the date as a string
-var n = date.toDateString();
-// get the time as a string
-var time = date.toLocaleTimeString();
 
-// find the html element with the id of time
-// set the innerHTML of that element to the date a space the time
+var n = date.toDateString();
+
+var time = date.toLocaleTimeString();
 document.getElementById('time').innerHTML = n + ' ' + time;
+today = new Date();
+currentMonth = today.getMonth();
+currentYear = today.getFullYear();
+selectYear = document.getElementById("year");
+selectMonth = document.getElementById("month");
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+function jump() {
+    currentYear = parseInt(selectYear.value);
+    currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+  let firstDay = (new Date(year, month)).getDay();
+   tbl = document.getElementById("calendar-body"); 
+    tbl.innerHTML = "";
+    monthAndYear.innerHTML = months[month] + " " + year;
+    selectYear.value = year;
+    selectMonth.value = month;
+   let date = 1;
+    for (let i = 0; i < 6; i++) {
+       let row = document.createElement("tr");
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                cell = document.createElement("td");
+                cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth(month, year)) {
+                break;
+            }
+
+            else {
+                cell = document.createElement("td");
+                cellText = document.createTextNode(date);
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
+                } 
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+        }
+
+        tbl.appendChild(row);
+    }
+}
+function daysInMonth(iMonth, iYear) {
+    return 32 - new Date(iYear, iMonth, 32).getDate();
+}
+
+
+
+
+
+
+
+
